5409fdc5fdf4cab92619bf56113007736468bc16
haileybeaz <bea13002@byui.edu>
2017-03-17 10:59:12 -0600

    fix

--- homework/jsonweather-html/scripts/modules.js
+++ homework/jsonweather-html/scripts/modules.js
@@ -1,7 +1,7 @@
 // Include modules into the html file
 // Pathes may need altered depending on site structure
 $(function () {
-  $("#page-header").load("/modules/header.html");
-  $("#page-nav").load("/modules/navigation.html");
-  $("#footer-content").load("/modules/footer.html");
+  $("#page-header").load("modules/header.html");
+  $("#page-nav").load("modules/navigation.html");
+  $("#footer-content").load("modules/footer.html");
 });
