0e2aa081e231c8ecb816a9a66417665024dee63a
haileybeaz <bea13002@byui.edu>
2017-04-04 20:15:00 -0600

    adding javascript

--- javascript/rexburg.js
+++ javascript/rexburg.js
@@ -1,1 +1,12 @@
-function init_map(){var myOptions = {zoom:13,center:new google.maps.LatLng(43.8231096,-111.79242369999997),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(43.8231096,-111.79242369999997)});infowindow = new google.maps.InfoWindow({content:'Rexburg, Idaho<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+jQuery(document).ready(function ($){
+    function ajaxFromLocalJson(locationToGrab){
+        $.ajax({
+            url:"/javascript/content.json",
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+            }
+        })
+    }
+    ajaxFromLocalJson('Food');
+});
